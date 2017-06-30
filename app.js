9b083a24c17c331dd9ac7d0c5570d2e725382bc0
Jon Olson <jonolson64@outlook.com>
2017-06-30 16:56:28 -0400

    add model directory and model file for campground

--- app.js
+++ app.js
@@ -1,21 +1,13 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"),
+    Campground  = require('./models/campground');
 
-mongoose.connect("mongodb://localhost/yelp_camp")
+mongoose.connect("mongodb://localhost/yelp_camp");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-
-// schema setup
-var campgroundSchema = new mongoose.Schema({
-  name: String,
-  image: String,
-  description: String
-});
-
-var Campground = mongoose.model("Campground", campgroundSchema);
 
 // Campground.create(
 //   {
