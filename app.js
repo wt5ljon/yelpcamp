c635dab015afb7189b3cde5550b9edc39de41cad
Jon Olson <jonolson64@outlook.com>
2017-07-03 07:11:25 -0400

    add styling to show page

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ mongoose.connect("mongodb://localhost/yelp_camp");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", function(req, res) {
   res.render("landing");
