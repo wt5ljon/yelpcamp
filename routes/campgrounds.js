f5d916b4767c4df6876869c4979e483b7e63f4a3
Jon Olson <jonolson64@outlook.com>
2017-07-07 10:52:46 -0400

    add delete campground route

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -80,6 +80,17 @@ router.put("/:id", function(req, res) {
   // redirect to show page
 });
 
+// DESTROY - campground route
+router.delete("/:id", function(req, res) {
+  Campground.findByIdAndRemove(req.params.id, function(error) {
+    if(error) {
+      res.redirect("/campgrounds");
+    } else {
+      res.redirect("/campgrounds");
+    }
+  });
+});
+
 // middleware
 function isLoggedIn(req, res, next) {
   if(req.isAuthenticated()) {
